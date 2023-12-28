c7706a74968d594ac417081d4df6465822488407
sukeypark <hspark@data-alliance.com>
2023-12-28 15:06:16 +0900

    update methods

--- src/com/example/device/CeilingFan.java
+++ src/com/example/device/CeilingFan.java
@@ -1,25 +1,41 @@
 package com.example.device;
 
 public class CeilingFan {
+    public static final int HIGH = 3;
+    public static final int MEDIUM = 2;
+    public static final int LOW = 1;
+    public static final int OFF = 0;
+
+    private String location;
+    private int speed;
+
+    public CeilingFan(String location) {
+        this.location = location;
+        speed = OFF;
+    }
 
     public void high() {
-
+        speed = HIGH;
     }
 
     public void medium() {
-
+        speed = MEDIUM;
     }
 
     public void low() {
-
+        speed = LOW;
     }
 
     public void off() {
-
+        speed = OFF;
     }
 
-    public void getSpeed() {
+    public int getSpeed() {
+        return speed;
+    }
 
+    public String getLocation() {
+        return location;
     }
 
 }
