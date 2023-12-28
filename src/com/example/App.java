ec610c28ccf2360adbe32b708439a365daaf5d58
sukeypark <hspark@data-alliance.com>
2023-12-28 14:27:38 +0900

    add app code

--- src/com/example/App.java
+++ src/com/example/App.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import com.example.command.Command;
+import com.example.command.LightOnCommand;
+import com.example.device.Light;
+import com.example.invoker.RemoteControl;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Command command = new LightOnCommand(new Light());
+        RemoteControl rc = new RemoteControl();
+
+        rc.setCommand(command);
+        rc.buttonWasPressed();
     }
 }
