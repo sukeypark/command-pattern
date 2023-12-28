f1b0f1d1fd981f9a28ad4d43a522bd667cce8ada
sukeypark <hspark@data-alliance.com>
2023-12-28 14:36:56 +0900

    add multi slots rc

--- src/com/example/invoker/RemoteControl.java
+++ src/com/example/invoker/RemoteControl.java
@@ -1,15 +1,33 @@
 package com.example.invoker;
 
 import com.example.command.Command;
+import com.example.command.NoCommand;
 
 public class RemoteControl {
-    Command slot;
+    Command[] onCommands;
+    Command[] offCommands;
 
-    public void setCommand(Command command) {
-        slot = command;
+    public RemoteControl() {
+        onCommands = new Command[7];
+        offCommands = new Command[7];
+
+        Command noCommand = new NoCommand();
+        for (int i = 0; i < 7; i++) {
+            onCommands[i] = noCommand;
+            offCommands[i] = noCommand;
+        }
     }
 
-    public void buttonWasPressed() {
-        slot.execute();
+    public void setCommand(int slot, Command onCommand, Command offCommand) {
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+
+    public void onButtonWasPressed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPressed(int slot) {
+        offCommands[slot].execute();
     }
 }
